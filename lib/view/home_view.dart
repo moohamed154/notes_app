a3882689c09140c980a3f70d6121bed0e432ea99
moohamed154 <medohs770@gmail.com>
2024-07-19 15:58:47 +0300

    change theme

---
+++ lib/view/home_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NotesVeiw extends StatelessWidget {
+  const NotesVeiw({super.key});
+
+//static String id = 'notes view';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
