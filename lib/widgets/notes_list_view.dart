49b1e7e66f9f25d57a0c15b4e5c40fed4f2095c9
moohamed154 <medohs770@gmail.com>
2024-07-24 17:05:11 +0300

    update ui to show the latest note at the top

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -4,6 +4,7 @@ import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/note_item.dart';
 
+//worked
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
 
@@ -11,26 +12,31 @@ class NotesListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<NotesCubit, NotesState>(
       builder: (context, state) {
-        List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes!;
+        // Fetch notes from the NotesCubit
+        List<NoteModel> notes =
+            BlocProvider.of<NotesCubit>(context).notes ?? [];
+
+        // Reverse the list to show the latest notes on top
+        final reversedNotes = notes.reversed.toList();
 
         return Padding(
           padding: const EdgeInsets.symmetric(
             vertical: 16,
           ),
           child: ListView.builder(
-              reverse: true,
-              itemCount: notes.length,
-              padding: EdgeInsets.zero,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 4,
-                  ),
-                  child: NotesItem(
-                    note: notes[index],
-                  ),
-                );
-              }),
+            itemCount: reversedNotes.length,
+            padding: EdgeInsets.zero,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 4,
+                ),
+                child: NotesItem(
+                  note: reversedNotes[index],
+                ),
+              );
+            },
+          ),
         );
       },
     );
